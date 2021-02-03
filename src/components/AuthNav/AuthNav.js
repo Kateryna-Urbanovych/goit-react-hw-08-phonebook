9a4d27d04c024f29c76845ec3688c1611afe0865
Kateryna-Urbanovych <katya_urb@ukr.net>
2021-02-03 02:16:50 +0200

    добавляет AppBar с Material-UI

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,23 +1,38 @@
 import { NavLink } from 'react-router-dom';
 import s from './AuthNav.module.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    link: {
+        margin: theme.spacing(1, 1.5),
+    },
+}));
 
 const AuthNav = () => {
+    const classes = useStyles();
+
     return (
         <div>
-            <NavLink
-                to="/register"
-                className={s.link}
-                activeClassName={s.activeLink}
-            >
-                Регистрация
-            </NavLink>
-            <NavLink
-                to="/login"
-                className={s.link}
-                activeClassName={s.activeLink}
-            >
-                Логин
-            </NavLink>
+            <Button color="primary" variant="outlined" className={classes.link}>
+                <NavLink
+                    to="/register"
+                    className={s.link}
+                    activeClassName={s.activeLink}
+                >
+                    Регистрация
+                </NavLink>
+            </Button>
+
+            <Button color="primary" variant="outlined" className={classes.link}>
+                <NavLink
+                    to="/login"
+                    className={s.link}
+                    activeClassName={s.activeLink}
+                >
+                    Логин
+                </NavLink>
+            </Button>
         </div>
     );
 };
--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -4,11 +4,11 @@ import phoneBookHome from 'images/phone-book.jpg';
 const HomeView = () => {
     return (
         <div className={s.container}>
-            <h1 className={s.title}>Главная страница нашего сервиса</h1>
+            <h1 className={s.title}>Телефонная книга</h1>
             <img
                 src={phoneBookHome}
                 alt="Главная страница нашего сервиса"
-                width="500"
+                width="600"
             />
         </div>
     );
